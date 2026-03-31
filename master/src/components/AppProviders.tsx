c83bcf06167ece35259e644228ea1d646c0830fc
Maciej Gren <maciej.gren@gmail.com>
2026-03-31 09:39:17 +0200

    feat(master): add standalone example app scaffold

---
+++ master/src/components/AppProviders.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import type { ReactNode } from 'react'
+import type { Locale } from '@open-mercato/shared/lib/i18n/config'
+import type { Dict } from '@open-mercato/shared/lib/i18n/context'
+import { I18nProvider } from '@open-mercato/shared/lib/i18n/context'
+import { ThemeProvider, FrontendLayout, QueryProvider, AuthFooter } from '@open-mercato/ui'
+import { ClientBootstrapProvider } from '@/components/ClientBootstrap'
+import { GlobalNoticeBars } from '@/components/GlobalNoticeBars'
+import { ComponentOverridesBootstrap } from '@/components/ComponentOverridesBootstrap'
+
+type AppProvidersProps = {
+  children: ReactNode
+  locale: Locale
+  dict: Dict
+  demoModeEnabled: boolean
+}
+
+export function AppProviders({ children, locale, dict, demoModeEnabled }: AppProvidersProps) {
+  return (
+    <I18nProvider locale={locale} dict={dict}>
+      <ClientBootstrapProvider>
+        <ComponentOverridesBootstrap>
+          <ThemeProvider>
+            <QueryProvider>
+              <FrontendLayout footer={<AuthFooter />}>{children}</FrontendLayout>
+              <GlobalNoticeBars demoModeEnabled={demoModeEnabled} />
+            </QueryProvider>
+          </ThemeProvider>
+        </ComponentOverridesBootstrap>
+      </ClientBootstrapProvider>
+    </I18nProvider>
+  )
+}
